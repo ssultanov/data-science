602e0db55497c677b3308c7994f4e645c20b0c68
ssultanov <58183086+ssultanov@users.noreply.github.com>
2020-05-23 10:23:53 -0400

    Add files via upload

---
+++ daily_return.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from pandas_datareader import data as wb
+from sys import argv
+
+def simple_return(start, ticker, *args):
+    stock_data = wb.DataReader(ticker, data_source='yahoo', start=start)
+    data = (stock_data['Adj Close'] / stock_data['Adj Close'].shift(1)) - 1
+    file_name = 'daily_return_' + str(ticker) + '.csv'
+    data.to_csv(file_name)
+
+simple_return(*argv[1:])
